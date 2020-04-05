c65b9cb6d682657391c5e6ae6d092c1fc287e563
kapitan21099 <kapitan21099@gmail.com>
2020-04-05 16:49:24 +0300

    add fix

---
+++ KI_TestPlugin/DeactiveCE_KI.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace UDS.TestEduPlugin
+{
+	public class DeactiveCE_KI : Plugin
+	{
+		public DeactiveCE_KI() : base(typeof(DeactiveCE_KI))
+		{
+			base.RegisteredEvents
+				.Add(new Tuple<int, string, string, Action<LocalPluginContext>>((int)PluginStage.PostOperation, "new_DeactivChaild_KI",
+                    "new_ki_n_n",
+					ChangeLookDeactiveChildEntity));
+		}
+
+		protected void ChangeLookDeactiveChildEntity(LocalPluginContext localContext)
+		{
+			IOrganizationService service = localContext.OrganizationService;
+			if (!localContext.PluginExecutionContext.InputParameters.Contains("deactiv_chaild_KI"))
+			{
+				throw new InvalidPluginExecutionException($"{nameof(localContext)} doesn`t contain 'deactiv_chaild_KI'");
+			}
+			var entCollChaild = (EntityCollection)localContext.PluginExecutionContext.InputParameters["deactiv_chaild_KI"];
+            if (entCollChaild.Entities.Count != 0)
+            {
+                for (int i = 0; i < entCollChaild.Entities.Count; i++)
+                {
+                    DeactivateRecord(entCollChaild[i].LogicalName, entCollChaild[i].Id, service);
+                }
+
+
+            }
+        }
+        public static void DeactivateRecord(string entityName, Guid recordId, IOrganizationService organizationService)
+        {
+            var cols = new ColumnSet(new[] { "statecode", "statuscode" });
+            //Check if it is Active or not
+            var entity = organizationService.Retrieve(entityName, recordId, cols);
+            if (entity != null && entity.GetAttributeValue<OptionSetValue>("statecode").Value == 0)
+            {
+                //StateCode = 1 and StatusCode = 2 for deactivating Account or Contact
+                SetStateRequest setStateRequest = new SetStateRequest()
+                {
+                    EntityMoniker = new EntityReference
+                    {
+                        Id = recordId,
+                        LogicalName = entityName,
+                    },
+                    State = new OptionSetValue(1),
+                    Status = new OptionSetValue(2)
+                };
+                organizationService.Execute(setStateRequest);
+            }
+        }        
+    }
+	}
--- KI_TestPlugin/ForSome.cs
+++ KI_TestPlugin/ForSome.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Crm.Sdk.Messages;
 
 namespace UDS.TestEduPlugin
 {
-    public class ForSome : Plugin
+    public class ForSome: Plugin
     {
         public ForSome()
             : base(typeof(ForSome))
@@ -14,64 +15,64 @@ namespace UDS.TestEduPlugin
                     "new_ki_task_3",
                     ChangeLookUpFields));
         }
+        private IOrganizationService _service;
 
-        protected void ChangeLookUpFields(LocalPluginContext localContext)
+        public static int StateCode { get; private set; }
+
+        //public ForSome(IOrganizationService service)
+        //{
+        //    _service = service;
+        //}
+        public void MainMethSome()
         {
-            if (!localContext.PluginExecutionContext.InputParameters.Contains("Target"))
-            {
-                throw new InvalidPluginExecutionException($"{nameof(localContext)} doesn`t contain 'Target'");
-
-            }///////////////////////////
-            IOrganizationService service = localContext.OrganizationService;
-             // Define Condition Values
-            var lookUpId = "4e4fffc8-5257-ea11-8125-00155d06f203";
-            var dayCount = 2;
-            var idN_N_Record = "2cbcc367-ae6e-ea11-8125-00155d06f203";
-
-            // Instantiate QueryExpression QEnew_l_ki
+            //Guid guid = new Guid(guid);
+            var QEnew_l_ki_new_new_l_ki_new_ki_n_n_new_ki_n_n_new_ki_n_nid = "ad34c56c-aa6e-ea11-8125-00155d06f203";
             var qe = new QueryExpression("new_l_ki");
             qe.TopCount = 50;
-            // Add columns to QEnew_l_ki.ColumnSet
-            qe.ColumnSet.AddColumns("new_name", "new_look_up_on_account_l_ki", "createdon", "new_l_kiid");
-            // Define filter QEnew_l_ki.Criteria
-            qe.Criteria.AddCondition("new_look_up_on_account_l_ki", ConditionOperator.Equal, lookUpId);
-            qe.Criteria.AddCondition("createdon", ConditionOperator.OlderThanXDays, dayCount);
-            // Add link-entity QEnew_l_ki_new_new_l_ki_new_ki_n_n
+            qe.ColumnSet.AddColumns("new_name", "new_l_kiid");
             var QEnew_l_ki_new_new_l_ki_new_ki_n_n = qe.AddLink("new_new_l_ki_new_ki_n_n", "new_l_kiid", "new_l_kiid");
-            // Add link-entity QEnew_l_ki_new_new_l_ki_new_ki_n_n_new_ki_n_n
             var QEnew_l_ki_new_new_l_ki_new_ki_n_n_new_ki_n_n = QEnew_l_ki_new_new_l_ki_new_ki_n_n.AddLink("new_ki_n_n", "new_ki_n_nid", "new_ki_n_nid");
+            QEnew_l_ki_new_new_l_ki_new_ki_n_n_new_ki_n_n.LinkCriteria.AddCondition("new_ki_n_nid", ConditionOperator.Equal, QEnew_l_ki_new_new_l_ki_new_ki_n_n_new_ki_n_n_new_ki_n_nid);
+            EntityCollection cont = _service.RetrieveMultiple(qe);
+            if (cont.Entities.Count != 0)
+            {
+                for (int i = 0; i < cont.Entities.Count; i++)
+                {
+                    DeactivateRecord(cont[i].LogicalName, cont[i].Id, _service);
+                }
 
-            // Define filter QEnew_l_ki_new_new_l_ki_new_ki_n_n_new_ki_n_n.LinkCriteria
-            QEnew_l_ki_new_new_l_ki_new_ki_n_n_new_ki_n_n.LinkCriteria.AddCondition("new_ki_n_nid", ConditionOperator.Equal, idN_N_Record);
-            EntityCollection contacts = service.RetrieveMultiple(qe);
-            //////////////////////////
-            Entity target = (Entity)localContext.PluginExecutionContext.InputParameters["Target"];
-            Entity parentCustonerId, primaryContactId;
-            EntityReference lookupOnContact, lookupOnAccaunt;
-            if (target.Contains("new_look_up_on_contact"))
-                lookupOnContact = target.GetAttributeValue<EntityReference>("new_look_up_on_contact");
-            else
-                lookupOnContact = null;
-            if (target.Contains("new_look_up_onaccaunt"))
-                lookupOnAccaunt = target.GetAttributeValue<EntityReference>("new_look_up_onaccaunt");
-            else
-                lookupOnAccaunt = null;
-            if (lookupOnContact is null)
-            {
-                parentCustonerId = service.Retrieve(lookupOnContact.LogicalName, lookupOnContact.Id, new ColumnSet("parentcustomerid1"));
-                if (parentCustonerId is null)
-                    target["new_look_up_onaccaunt"] = null;
-                else
-                    target["new_look_up_onaccaunt"] = parentCustonerId.GetAttributeValue<EntityReference>("parentcustomerid1");
+
             }
-            else if (lookupOnAccaunt is null)
-            {
-                primaryContactId = service.Retrieve(lookupOnAccaunt.LogicalName, lookupOnAccaunt.Id, new ColumnSet("primarycontactid"));
-                if (primaryContactId is null)
-                    target["new_look_up_on_contact"] = null;
-                else
-                    target["new_look_up_on_contact"] = primaryContactId.GetAttributeValue<EntityReference>("primaryContactId");
-            }            
+
         }
-    }
+        public static void DeactivateRecord(string entityName, Guid recordId, IOrganizationService organizationService)
+        {
+            var cols = new ColumnSet(new[] { "statecode", "statuscode" });
+            //Check if it is Active or not
+            var entity = organizationService.Retrieve(entityName, recordId, cols);
+            if (entity != null && entity.GetAttributeValue<OptionSetValue>("statecode").Value == 0)
+            {
+                //StateCode = 1 and StatusCode = 2 for deactivating Account or Contact
+                SetStateRequest setStateRequest = new SetStateRequest()
+                {
+                    EntityMoniker = new EntityReference
+                    {
+                        Id = recordId,
+                        LogicalName = entityName,
+                    },
+                    State = new OptionSetValue(1),
+                    Status = new OptionSetValue(2)
+                };
+                organizationService.Execute(setStateRequest);
+            }
+        }
+        protected void ChangeLookUpFields(LocalPluginContext localContext)
+        { }
+       
+    }///////////////////////////
+     
+
+
+
 }
+
